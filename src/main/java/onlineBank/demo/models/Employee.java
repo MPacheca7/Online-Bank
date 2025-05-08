525a7ab2f77cd0c2b90387c110bc5bf528098839
Pacheca <mpacheca7@gmail.com>
2025-05-08 12:03:56 +0200

    class employee

---
+++ src/main/java/onlineBank/demo/models/Employee.java
@@ -0,0 +1,31 @@
+package onlineBank.demo.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class Employee extends User{
+
+    @Id
+    @Column(name = "employee_number")
+    private int employeeNumber;
+
+    public Employee(String name, String dni, String email, Role role, int employeeNumber) {
+        super(name, dni, email, role);
+        this.employeeNumber = employeeNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeNumber=" + employeeNumber +
+                '}';
+    }
+}
